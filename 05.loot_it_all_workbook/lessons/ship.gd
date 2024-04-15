92146472b02524e4d4f32a5144ec80843c73504a
qzjo <jaocampo@s.sfusd.edu>
2024-04-15 14:12:09 -0700

    L9-Keep-the-ship-on-screen

--- 05.loot_it_all_workbook/lessons/ship.gd
+++ 05.loot_it_all_workbook/lessons/ship.gd
@@ -19,6 +19,11 @@ func set_health(new_health: int) -> void:
 	get_node("UI/HealthBar").value = health
 
 func _process(delta: float) -> void:
+	
+	var viewport_size := get_viewport_rect().size
+	position.x = wrapf(position.x, 0, viewport_size.x)
+	position.y = wrapf(position.y, 0, viewport_size.y)
+	
 	var direction := Vector2(0, 0)
 	direction.x = Input.get_axis("move_left", "move_right")
 	direction.y = Input.get_axis("move_up", "move_down")
