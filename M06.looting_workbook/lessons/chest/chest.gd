cb0e307c12aa65a0c849867961ea3a8dc1c99e43
qzjo <jaocampo@s.sfusd.edu>
2024-04-29 14:11:36 -0700

    L5-Open-the-chest

--- M06.looting_workbook/lessons/chest/chest.gd
+++ M06.looting_workbook/lessons/chest/chest.gd
@@ -1,10 +1,27 @@
 extends Area2D
 
-
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var canvas_group: = $CanvasGroup
 
 
+func _input_event(viewport: Node, event: InputEvent, shape_idx: int):
+	
+	var event_is_mouse_click: bool = (
+		event is InputEventMouseButton and
+		event.button_index == MOUSE_BUTTON_LEFT and
+		event.is_pressed()
+	)
+	
+	if event_is_mouse_click:
+		open()
 
+
+func open() -> void:
+	animation_player.play("open")		
+	input_pickable = false
+	
+
+	
 func _ready():
 	mouse_entered.connect(_on_mouse_entered)
 	mouse_exited.connect(_on_mouse_exited)
--- M06.looting_workbook/practices/L5.P1.open_the_door/door.gd
+++ M06.looting_workbook/practices/L5.P1.open_the_door/door.gd
@@ -4,10 +4,12 @@ extends Area2D
 
 
 func _input_event(viewport: Node, event: InputEvent, shape_index: int):
+	animation_player.play("open")
 	# Complete this boolean expression to check if the input event is a left-mouse click.
 	var event_is_mouse_click: bool = (
 		false
 	)
+	input_pickable = false
 
 	if event_is_mouse_click:
 		pass
